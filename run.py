2337e86095e181e22987560e561a72aea9db8127
rodolazo <rodolfo.lazo@yahoo.com>
2021-11-28 05:15:53 +0000

    Get a random card

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-print("Everything working")
+import random
+
+def system_card():
+  """Get a random card from list"""
+  list_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(list_cards)
+  return card
+
+
+print(system_card())
+
 
 
