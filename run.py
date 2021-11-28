589f8c7dcf39436b136009462eb12baf2a02519a
rodolazo <rodolfo.lazo@yahoo.com>
2021-11-28 08:11:09 +0000

    Add logo and main function to repeat the game

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import random
+import os
+from design import texto
 
 def random_card():
   """Get a random card from list"""
@@ -17,39 +19,78 @@ def get_score(list_cards):
 
     return sum(list_cards)
 
-#Get two cards using the function random_card()
-mycards_list = []
-system_list = []
-is_over = False
+def compare_score(my_score, system_score):
+  """ Compares system score and personal score and decide who wins"""
+  if my_score > 21 and system_score > 21:
+    return "You lose. You went over"
 
-for item in range(2):
-        mycards_list.append(random_card())
-        system_list.append(random_card())
+  if my_score == system_score:
+    return "Draw"  
+  elif my_score == 0:
+    return "You win, you got a 21"
+  elif my_score > 21:
+    return "You lose, you went over 21"
+  elif system_score == 0:
+      return "You lose, the system got a 21"  
+  elif system_score > 21:
+    return "You win, the system went over 21"
+  elif my_score > system_score:
+    return "You win"
+  else:
+    return "You lose"
 
-# Let the user to ask for another card and stop the game when is 21 or over
-while not is_over:
-    mycards_score = get_score(mycards_list)
-    system_score = get_score(system_list)
+def screen_clear():
+   # Clean console screen 
+   # for mac and linux(here, os.name is 'posix')
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      # for windows platfrom
+      _ = os.system('cls')
 
-    print(f"The first card of the system is {system_list[0]}")
-    print(f"My cards are: {mycards_list} and my score is {mycards_score}")
+def main():
+    #Get two cards using the function random_card()
+    mycards_list = []
+    system_list = []
+    is_over = False
 
-    if mycards_score == 0 or mycards_score >21 or system_score == 0:
-        is_over = True
-    else:
-        ask_card = input("Type 'y' to get another card or 'n' to finish the game: ")
-        if ask_card == 'y':
+    for item in range(2):
             mycards_list.append(random_card())
-        else:
+            system_list.append(random_card())
+
+    # Let the user to ask for another card and stop the game when is 21 or over
+    while not is_over:
+        mycards_score = get_score(mycards_list)
+        system_score = get_score(system_list)
+
+        print(f"The first card of the system is {system_list[0]}")
+        print(f"My cards are: {mycards_list} and my score is {mycards_score}")
+
+        if mycards_score == 0 or mycards_score >21 or system_score == 0:
             is_over = True
+        else:
+            ask_card = input("Type 'y' to get another card or 'n' to finish the game: ").lower()
+            if ask_card == 'y':
+                mycards_list.append(random_card())
+            else:
+                is_over = True
 
-#If the system score is less than 17 thenhas to deal a new card
-while system_score != 0 and system_score < 17:
-    system_list.append(random_card())
-    system_score = get_score(system_list)
+    #If the system score is less than 17 then has to deal a new card
+    while system_score != 0 and system_score < 17:
+        system_list.append(random_card())
+        system_score = get_score(system_list)
+
+    #Print Final result
+    print(f"They system cards are: {system_list} and its score is: {system_score}")
+    print(f"Your cards are: {mycards_list} and your score is: {mycards_score}")
+    print(compare_score(mycards_score, system_score))
 
 
-
+#Ask to play the game again
+while input("Would you like to play 21 game? <y> or <n>: ").lower() == 'y':
+    screen_clear()
+    print(texto)
+    main()
 
 
 
